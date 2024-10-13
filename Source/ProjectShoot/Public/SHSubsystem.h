9475c0d1723733a5f596fe51c9f8086a93ba13dc
john9590 <john9590@gmail.com>
2024-10-13 21:02:08 +0900

    For Code Delete

---
+++ Source/ProjectShoot/Public/SHSubsystem.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "SHSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSHOOT_API USHSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
